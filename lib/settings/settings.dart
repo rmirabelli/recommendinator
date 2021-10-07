537cdd62239ec4d57b2095655a020c4df9bae965
Ryan Gant <ryan.gant@bottlerocketstudios.com>
2021-10-07 14:51:11 -0500

    Added settings items.

--- lib/settings/settings.dart
+++ lib/settings/settings.dart
@@ -3,6 +3,8 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:recommendinator/settings/bloc/settings_bloc.dart';
 
+enum SettingsEnum { addItem, reset }
+
 class Settings extends StatelessWidget {
   const Settings({Key? key}) : super(key: key);
 
@@ -12,9 +14,50 @@ class Settings extends StatelessWidget {
       appBar: AppBar(title: const Text('Settings')),
       body: BlocBuilder<SettingsBloc, SettingsState>(
         builder: (context, state) {
-          return Container();
+          return _settingsContent(context, state);
         },
       ),
     );
+  }
+
+  Widget _settingsContent(BuildContext context, SettingsState state) {
+    return ListView.builder(
+        itemCount: SettingsEnum.values.length,
+        itemBuilder: (context, index) {
+          var setting = SettingsEnum.values[index];
+          return Card(
+              child: ListTile(
+                  onTap: () => _performActionForSetting(setting),
+                  title: Text(_titleForSetting(setting)),
+                  subtitle: Text(_subtitleForSetting(setting))));
+        });
+  }
+
+  void _performActionForSetting(SettingsEnum setting) {
+    //TODO: Actually make them do stuff
+    switch (setting) {
+      case SettingsEnum.addItem:
+        break;
+      case SettingsEnum.reset:
+        break;
+    }
+  }
+
+  String _titleForSetting(SettingsEnum setting) {
+    switch (setting) {
+      case SettingsEnum.addItem:
+        return "Add Menu Item";
+      case SettingsEnum.reset:
+        return "Reset App";
+    }
+  }
+
+  String _subtitleForSetting(SettingsEnum setting) {
+    switch (setting) {
+      case SettingsEnum.addItem:
+        return "Add a new menu item to the food database";
+      case SettingsEnum.reset:
+        return "Resets all menu items and food recommendations";
+    }
   }
 }
